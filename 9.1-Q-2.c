8a54b122ca466d5b02c0498707295d6d3746c85f
Harwina <164463440+Harwina@users.noreply.github.com>
2024-05-03 23:23:17 +0530

    Add files via upload

---
+++ 9.1-Q-2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+	char a[100];
+	int i;
+	
+	printf("enter any string=");
+	gets(a);
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i]>=65 && a[i]<=90)
+		{
+			a[i]=a[i]+32;
+		}
+	}
+	printf("uppercase string=%s",a);
+}
